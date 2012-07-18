2a807d5ca2d735f4f39c7c0d15f6f6e935184f27
Andrew Hudson <ahudson@gmail.com>
2012-07-18 22:29:09 +0100

    updating theme to fit how live site is.

---
+++ wp-content/themes/bigandy/footer.php
@@ -0,0 +1,20 @@
+</div><!--end of .container-->
+<footer id="footer">
+	<p>&copy;2004-<?php echo date('Y');?> Andrew JD Hudson . <a href="mailto:andy@big-andy.co.uk">email me</a></p>
+	</footer><!--end of footer-->
+</div><!--end of container-->
+<!--google tracking javascript-->
+<script src="<?php bloginfo( 'template_url' ); ?>/js/typekit.min.js"></script>
+<script>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-6954334-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</body>
+</html>
--- wp-content/themes/bigandy/index.php
+++ wp-content/themes/bigandy/index.php
@@ -1,48 +1,38 @@
-<?php $thisPage="Home"; ?>
-<?php include 'php/header.php';?>
-		<!--main section of text-->
-	<section id="content-main">
-		<h1>welcome to big-andy.co.uk</h1>
-		<p>Greetings and I hope that you're well! I am Andrew JD Hudson aka bigandy and this is my personal site where I hope to showcase my burgeoning web-design skills.</p>
-		
-			<p>While you're here you can can check out my 
-			<a href="/photos/index.php" title="photos">photos</a>, 
-			<a href="http://www.blog.big-andy.co.uk" title="blog">blog</a>,
-			and find out how to <a href="/contact/index.php" title="contact">contact</a> me.
-			</p>
-			<p>First of all you might want to find what to read <a href="/about/index.php" title="about">about</a> me.</p>
-			<img src="/big-andy.co.uk/img/bigandy.jpg" alt="image of bigandy" id="photo-me" height="350" width="522" />
-	
-	</section><!--end of content-main-->
-	<aside id="sub-content">
-
-		<section id="blog-posts">
-			<h2><a href="http://blog.big-andy.co.uk/" title="visit my blog">Blog</a></h2>
-			<h3>latest posts</h3>
-			<!-- php to get top 3 blog posts from wordpress blog; returns 'have a nice day' if on localhost -->
-			<?php
-			if ($_SERVER["SERVER_NAME"] == "localhost") {echo "<p>this is a nice day</p>";}
-			else {include 'php/latest-blog-posts.php';}
-			?>
-		</section><!--end of blog posts-->
-	
-	
-	</aside><!--end of sub-content-->
-	<!-- twitter; put at the bottom to speed up page-load-->
-	<section id="twitter">
-		<script src="/big-andy.co.uk/js/typekit-twitter.min.js"></script>
-	</section>
-	<!--end of twitter-->
-	
-			<!--footer text-->
-	<footer id="footer">
-		<p>&copy;2004-<?php echo date('Y');?> Andrew JD Hudson . <a href="mailto:andy@big-andy.co.uk">email me</a></p>
-	</footer><!--end of footer-->
-</div><!--end of container-->
-<!--google tracking javascript-->
 <?php
-include'php/google-code.php';
-?>
-</body>
-</html>
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query. 
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Boilerplate
+ * @since Boilerplate 1.0
+ */
+
+get_header(); ?>
+<section>
+	<?php if ( have_posts() ) : ?>
+
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php the_content(); ?>
+
+				<?php endwhile; ?>
+
+				
+
+			
+
+			
+
+			<?php endif; ?>
+</section>
+<?php get_footer(); ?>
+
 
