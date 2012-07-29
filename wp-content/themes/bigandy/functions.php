c066bd98b04bdeeac8114bbf6115ab95e0a69148
Andrew Hudson <ahudson@gmail.com>
2012-07-29 19:32:07 +0100

    hiding some sections in the admin area!

--- wp-content/themes/bigandy/functions.php
+++ wp-content/themes/bigandy/functions.php
@@ -1,7 +1,23 @@
 <?php 
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menu( 'primary', __( 'Primary Menu', 'bigandy' ) );
+// This theme uses wp_nav_menu() in one location.
+register_nav_menu( 'primary', __( 'Primary Menu', 'bigandy' ) );
 
-	// Add support for a variety of post formats
-	add_theme_support( 'post-formats', array( 'aside', 'link', 'gallery', 'status', 'quote', 'image' ) );
+// Add support for a variety of post formats
+add_theme_support( 'post-formats', array( 'aside', 'link', 'gallery', 'status', 'quote', 'image' ) );
+
+
+
+// http://www.wprecipes.com/how-to-remove-menus-in-wordpress-dashboard
+	// see also : http://noeltock.com/wcuk12/
+
+function remove_menus () {
+global $menu;
+	$restricted = array( __('Media'), __('Links'), __('Tools'), __('Users'), __('Comments'), __('Plugins'));
+	end ($menu);
+	while (prev($menu)){
+		$value = explode(' ',$menu[key($menu)][0]);
+		if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+	}
+}
+add_action('admin_menu', 'remove_menus');
