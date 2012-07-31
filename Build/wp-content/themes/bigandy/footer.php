328c6b101b88d1a0fc21398b0e7fbfd1dee380b2
Andrew Hudson <ahudson@gmail.com>
2012-07-31 20:34:33 +0100

    compartmentalising plugin

--- Build/wp-content/themes/bigandy/footer.php
+++ Build/wp-content/themes/bigandy/footer.php
@@ -1,8 +1,7 @@
 <footer id="footer">
-	<!--temp site login-->
-	<a href="/wp-admin">admin</a>
 	<!--END-->
-	<p>&copy;2004-<?php echo date('Y');?> Andrew JD Hudson . <a href="mailto:andy@big-andy.co.uk">email me</a></p>
+	<p>&copy;2004-<?php echo date('Y');?> Andrew JD Hudson</p>
+	<p>Powered by <a href="http://wordpress.org" target="_blank" title="link opens in new window - WordPress">WordPress</a></p>
 	</footer><!--end of footer-->
 </div><!--end of container-->
 <!--google tracking javascript-->
