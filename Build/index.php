c4ab0869b13a520cc54ab9e5e3d48fa0618f91f7
Andrew Hudson <ahudson@gmail.com>
2012-08-29 21:11:05 +0100

    Moving to WP Skeleton inspired structuring + _S

--- Build/index.php
+++ Build/index.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+// WordPress view bootstrapper
+define( 'WP_USE_THEMES', true );
+require( './wp/wp-blog-header.php' );
