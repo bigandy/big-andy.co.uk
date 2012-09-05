383ca67c7f978ac01670314a47b779eed05d485f
Andrew Hudson <ahudson@gmail.com>
2012-09-05 14:13:23 +0100

    added current_page_parent to included classes

--- content/plugins/bigandy/remove_menu_classes.php
+++ content/plugins/bigandy/remove_menu_classes.php
@@ -5,14 +5,14 @@
 add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
 
 function my_css_attributes_filter($var) {
-  return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
-}
+  return is_array($var) ? array_intersect($var, array('current-menu-item', 'current_page_parent')) : '';
+}
 
 // Strip Classes, except for .current-menu-item
 // http://stackoverflow.com/a/8777624/965191
 
 add_filter ('wp_nav_menu','strip_empty_classes');
-function strip_empty_classes($menu) {
-    $menu = preg_replace('/ class=(["\'])(?!current-menu-item).*?\1/','',$menu);
-    return $menu;
-}
+ function strip_empty_classes($menu) {
+     $menu = preg_replace('/ class=(["\'])(?!current-menu-item || current_page_parent ).*?\1/','',$menu);
+     return $menu;
+ }
