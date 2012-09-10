edf426b77cdbcac1afaae0addc84d63c92b14985
Andrew Hudson <ahudson@gmail.com>
2012-09-10 22:24:30 +0100

    better way of removing height/width attribs
    http://wordpress.stackexchange.com/questions/29881/stop-wordpress-from-hardcoding-img-width-and-height-attributes

--- content/plugins/bigandy/images.php
+++ content/plugins/bigandy/images.php
@@ -1,13 +1,4 @@
 <?php
-// remove height width attributes from images : 
-// http://css-tricks.com/snippets/wordpress/remove-width-and-height-attributes-from-inserted-images/
-add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
-add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
-
-function remove_width_attribute( $html ) {
-   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
-   return $html;
-}
 
 /* Changing the [gallery] shortcode */
 
@@ -112,4 +103,48 @@ function gallery_shortcode_andrew($attr) {
 	return $output;
 }
 
+// Altering the image_downsize function 
+// http://wordpress.stackexchange.com/questions/29881/stop-wordpress-from-hardcoding-img-width-and-height-attributes
+
+function ah_image_downsize( $value = false, $id, $size ) {
+    if ( !wp_attachment_is_image($id) )
+        return false;
+
+    $img_url = wp_get_attachment_url($id);
+    $is_intermediate = false;
+    $img_url_basename = wp_basename($img_url);
+
+    // try for a new style intermediate size
+    if ( $intermediate = image_get_intermediate_size($id, $size) ) {
+        $img_url = str_replace($img_url_basename, $intermediate['file'], $img_url);
+        $is_intermediate = true;
+    }
+    elseif ( $size == 'thumbnail' ) {
+        // Fall back to the old thumbnail
+        if ( ($thumb_file = wp_get_attachment_thumb_file($id)) && $info = getimagesize($thumb_file) ) {
+            $img_url = str_replace($img_url_basename, wp_basename($thumb_file), $img_url);
+            $is_intermediate = true;
+        }
+    }
+
+    // We have the actual image size, but might need to further constrain it if content_width is narrower
+    if ( $img_url) {
+        return array( $img_url, 0, 0, $is_intermediate );
+    }
+    return false;
+}
+
+add_filter( 'image_downsize', 'ah_image_downsize', 1, 3 );
+
 /* END [gallery] */
+
+// remove height width attributes from images : 
+// http://css-tricks.com/snippets/wordpress/remove-width-and-height-attributes-from-inserted-images/
+/*
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+}*/
