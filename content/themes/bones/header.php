977248aaf0ba6a291a57957bdc3180565623c1c3
Andrew Hudson <ahudson@gmail.com>
2012-09-26 12:58:05 +0100

    update styles when .dark is active

--- content/themes/bones/header.php
+++ content/themes/bones/header.php
@@ -1,9 +1,9 @@
 <!doctype html>  
 
-<!--[if lt IE 7]><html manifest="<?php bloginfo('home'); ?>/cache_manifest" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <html manifest="<?php bloginfo('home'); ?>/cache_manifest" class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html manifest="<?php bloginfo('home'); ?>/cache_manifest" class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html manifest="<?php bloginfo('home'); ?>/cache_manifest" class="no-js"><!--<![endif]-->
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"><!--<![endif]-->
 	
 	<head>
 		<meta charset="utf-8">
