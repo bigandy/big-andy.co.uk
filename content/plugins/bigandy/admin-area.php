ce1d0d32ab4bb8d9d9385ee0603334e2fb96eb5d
Andrew Hudson <ahudson@gmail.com>
2012-10-07 17:11:58 +0100

    remove plugins, cpt
    and also allow user to edit media and upload files

--- content/plugins/bigandy/admin-area.php
+++ content/plugins/bigandy/admin-area.php
@@ -28,8 +28,8 @@ if( $options['admin'] == "Y") {
 
     $role = get_role('administrator');
 			     
-			$role->remove_cap('upload_files');
-			$role->remove_cap('manage_galleries');
+			$role->add_cap('upload_files');
+			$role->add_cap('manage_galleries');
 			$role->remove_cap('export');
 			$role->remove_cap('import');
 			$role->remove_cap('manage_links');
