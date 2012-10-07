81477aea8a7af1380c725befbf30f954fb7184f2
Andrew Hudson <ahudson@gmail.com>
2012-10-07 13:12:20 +0100

    verson 1.2. only output ga code if is set.

--- content/plugins/bigandy/ah-footer.php
+++ content/plugins/bigandy/ah-footer.php
@@ -3,18 +3,20 @@ function ah_footer() {
 	$ah_options = get_option( 'ah_plugin_options' );
 	$ga_code = $ah_options['output'];
 
-	echo "<!-- google analytics code -->
-	<script type='text/javascript'>
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', '".$ga_code."']);
-	  _gaq.push(['_trackPageview']);
+	if( $ga_code != '') {
 
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>";
+		echo "<!-- google analytics code -->
+		<script type='text/javascript'>
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', '".$ga_code."']);
+		  _gaq.push(['_trackPageview']);
 
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>";
+	}
 }
 add_action('wp_footer', 'ah_footer');
