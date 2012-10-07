78a3e1b12bdb0713fd112cda42be49cdb1fa132a
Andrew Hudson <ahudson@gmail.com>
2012-10-07 12:06:09 +0100

    ability to set ga code with plugin variable

--- content/plugins/bigandy/ah-footer.php
+++ content/plugins/bigandy/ah-footer.php
@@ -1,6 +1,20 @@
 <?php 
 function ah_footer() {
-    echo '<script type="text/javascript" src="//use.typekit.net/avh3olg.js"></script>
-    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>';    
+	$ah_options = get_option( 'ah_plugin_options' );
+	$ga_code = $ah_options['output'];
+
+	echo "<!-- google analytics code -->
+	<script type='text/javascript'>
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', '".$ga_code."']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>";
+
 }
 add_action('wp_footer', 'ah_footer');
