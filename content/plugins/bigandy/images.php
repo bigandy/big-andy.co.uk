a7b81bdabf169a2aca431957beca780a3964f9d9
Andrew Hudson <ahudson@gmail.com>
2012-10-11 17:09:05 +0100

    update to latest beta nightly

--- content/plugins/bigandy/images.php
+++ content/plugins/bigandy/images.php
@@ -2,16 +2,31 @@
 
 /* Changing the [gallery] shortcode */
 
+/* see gallery_shortcode() in wp-includes/media.php */
+
 remove_shortcode( 'gallery', 'gallery_shortcode' );
-add_shortcode( 'gallery', 'gallery_shortcode_andrew' );
-function gallery_shortcode_andrew( $attr ) {
-	global $post, $wp_locale;
+
+add_shortcode('gallery', 'gallery_shortcode_ah_new');
+
+/**
+ * The Gallery shortcode.
+ *
+ * This implements the functionality of the Gallery Shortcode for displaying
+ * WordPress images on a post.
+ *
+ * @since 2.5.0
+ *
+ * @param array $attr Attributes of the shortcode.
+ * @return string HTML content to display gallery.
+ */
+function gallery_shortcode_ah_new($attr) {
+	$post = get_post();
 
 	static $instance = 0;
 	$instance++;
 
 	// Allow plugins/themes to override the default gallery template.
-	$output = apply_filters( 'post_gallery', '', $attr );
+	$output = apply_filters('post_gallery', '', $attr);
 	if ( $output != '' )
 		return $output;
 
@@ -22,86 +37,92 @@ function gallery_shortcode_andrew( $attr ) {
 			unset( $attr['orderby'] );
 	}
 
-	extract( shortcode_atts( array(
-				'order'      => 'ASC',
-				'orderby'    => 'title',
-				'id'         => $post->ID,
-				'itemtag'    => '',
-				'icontag'    => '',
-				'captiontag' => '',
-				'columns'    => 3,
-				'size'       => 'thumbnail',
-				'include'    => '',
-				'exclude'    => '',
-				'link'   => 'file'
-			), $attr ) );
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => '',
+		'icontag'    => '',
+		'captiontag' => '',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'ids'        => '',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr));
 
-	$id = intval( $id );
+	$id = intval($id);
 	if ( 'RAND' == $order )
 		$orderby = 'none';
 
-	if ( !empty( $include ) ) {
-		$include = preg_replace( '/[^0-9,]+/', '', $include );
-		$_attachments = get_posts( array( 'include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+	if ( !empty( $ids ) ) {
+		// 'ids' is explicitly ordered
+		$orderby = 'post__in';
+		$include = $ids;
+	}
+
+	if ( !empty($include) ) {
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
 
 		$attachments = array();
 		foreach ( $_attachments as $key => $val ) {
 			$attachments[$val->ID] = $_attachments[$key];
 		}
-	} elseif ( !empty( $exclude ) ) {
-		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
-		$attachments = get_children( array( 'post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+	} elseif ( !empty($exclude) ) {
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
 	} else {
-		$attachments = get_children( array( 'post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
 	}
 
-	if ( empty( $attachments ) )
+	if ( empty($attachments) )
 		return '';
 
 	if ( is_feed() ) {
 		$output = "\n";
 		foreach ( $attachments as $att_id => $attachment )
-			$output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
 		return $output;
 	}
 
-	$itemtag = tag_escape( $itemtag );
-	$captiontag = tag_escape( $captiontag );
-	$columns = intval( $columns );
-	$itemwidth = $columns > 0 ? floor( 100/$columns ) : 100;
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
 	$float = is_rtl() ? 'right' : 'left';
 
 	$selector = "gallery-{$instance}";
 
 	$gallery_style = $gallery_div = '';
-	if ( apply_filters( 'use_default_gallery_style', true ) )
-		$gallery_style = "";
 	$size_class = sanitize_html_class( $size );
-	$gallery_div = "<div class='gallery gallery-cols-{$columns}'>";
+	$gallery_div = "<div class='gallery-columns-{$columns}'>";
 	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
 
 	$i = 0;
 	foreach ( $attachments as $id => $attachment ) {
-		$link = wp_get_attachment_link( $id, $size, false, false );
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
 
-		// $output .= "<{$itemtag} class='gallery-item'>";
-		$output .= $link;
-		if ( $captiontag && trim( $attachment->post_excerpt ) ) {
+		
+		$output .= "
+			
+				$link
+			";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
 			$output .= "
 				<{$captiontag} class='wp-caption-text gallery-caption'>
-				" . wptexturize( $attachment->post_excerpt ) . "
+				" . wptexturize($attachment->post_excerpt) . "
 				</{$captiontag}>";
 		}
-		$output .= "</{$itemtag}>";
-
+		// $output .= "</{$itemtag}>";
+		/* if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<br style="clear: both" />'; */
 	}
 
-	$output .= "
-
-		</div>\n";
+	$output .= "</div>\n";
 
 	return $output;
 }
+
+
 
 // Altering the image_downsize function
 // http://wordpress.stackexchange.com/questions/29881/stop-wordpress-from-hardcoding-img-width-and-height-attributes
