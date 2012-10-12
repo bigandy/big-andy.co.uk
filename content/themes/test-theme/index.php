c008f927356a2167f2794ebea5ad432fda4cd85b
Andrew Hudson <ahudson@gmail.com>
2012-10-12 11:42:35 +0100

    added test-theme that uses grunt + compass!

---
+++ content/themes/test-theme/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <meta name="viewport" content="width=device-width" />
+        <title><?php wp_title(' | '); ?></title>
+        <?php wp_head(); ?>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"  />
+    </head>
+    <body>
+		
+	
+ 
+    <h1>Cobblers</h1>   
+        <?php wp_footer(); ?>
+	</body>
+</html>
