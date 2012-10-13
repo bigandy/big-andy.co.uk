5410e5b0bae48585aa3ce7fd8f029c82d972046c
Andrew Hudson <ahudson@gmail.com>
2012-10-13 19:30:35 +0100

    changing fonts to typekit fonts

--- content/themes/bones/header.php
+++ content/themes/bones/header.php
@@ -29,7 +29,7 @@
 			
 		<!-- drop Google Analytics Here -->
 		<!-- end analytics -->
-		<?php if( is_page_template('page-templates/thoughts.php' ) ) {  ?>
+		<?php // if( is_page_template('page-templates/thoughts.php' ) ) {  ?>
 			<script type="text/javascript">
 			  (function() {
 			    var config = {
@@ -39,7 +39,7 @@
 			    var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
 			  })();
 			</script>
-		<?php } ?>
+		<?php // } ?>
 	</head>
 	
 	<body <?php body_class(); ?>>
