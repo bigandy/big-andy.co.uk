9d20ec7571a8559322f0ec95a081c10058832b4a
Andrew Hudson <ahudson@gmail.com>
2012-10-13 19:49:47 +0100

    tidying up header.php

--- content/themes/bones/header.php
+++ content/themes/bones/header.php
@@ -1,69 +1,48 @@
-<!doctype html>  
-
+<!doctype html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"><!--<![endif]-->
+<!--[if gt IE 8]>
+<!--><html class="no-js" lang="en"><!--<![endif]-->
 	
-	<head>
-		<meta charset="utf-8">
+<head>
+	<meta charset="utf-8">		
+	<title>Bigandy.co.uk - Andrew Hudson's Online Home</title>
+
+	<script type="text/javascript">
+	  (function() {
+	    var config = {
+	      kitId: 'bhs0nmm',
+	      scriptTimeout: 3000
+	    };
+	    var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+	  })();
+	</script>
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">				
+	<?php wp_head(); ?>
+</head>
+	
+<body <?php // body_class(); ?>>
+	
+	<div id="container">
+		<header class="header" role="banner">
 		
-		<title><?php wp_title(''); ?></title>
-		
-		<!-- Google Chrome Frame for IE -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		
-		<!-- mobile meta (hooray!) -->
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		
-		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+			<div id="inner-header" class="wrap clearfix">					
 				
-  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+				<?php if( !is_page_template('page-templates/thoughts.php' ) ) {  ?>
+				
+				<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+				<?php  bloginfo('description'); ?>
+				
+				<nav role="navigation" class="nav top-nav clearfix">
+						<?php  bones_main_nav(); ?>						
+				</nav>
+				<?php } ?>
+				
+			</div> <!-- end #inner-header -->
 		
-		<!-- wordpress head functions -->
-		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
-			
-		<!-- drop Google Analytics Here -->
-		<!-- end analytics -->
-		<?php // if( is_page_template('page-templates/thoughts.php' ) ) {  ?>
-			<script type="text/javascript">
-			  (function() {
-			    var config = {
-			      kitId: 'bhs0nmm',
-			      scriptTimeout: 3000
-			    };
-			    var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
-			  })();
-			</script>
-		<?php // } ?>
-	</head>
-	
-	<body <?php body_class(); ?>>
-	
-		<div id="container">
-			<header class="header" role="banner">
-			
-				<div id="inner-header" class="wrap clearfix">
-					
-					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					
-					
-					<!-- if you'd like to use the site description you can un-comment it below -->
-					<?php // bloginfo('description'); ?>
-					
-					<?php if( !is_page_template('page-templates/thoughts.php' ) ) {  ?>
-					
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-					
-					<nav role="navigation" class="nav top-nav clearfix">
-							<?php  bones_main_nav(); ?>						
-					</nav>
-					<?php } ?>
-					
-				</div> <!-- end #inner-header -->
-			
-			</header> <!-- end header -->
+		</header> <!-- end header -->
