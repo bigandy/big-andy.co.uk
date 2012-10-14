82038246498594b07a60ecacb67e040c601e092d
Andrew Hudson <ahudson@gmail.com>
2012-10-14 20:40:57 +0100

    further page-home tweeks (3)

--- content/themes/bones/page-home.php
+++ content/themes/bones/page-home.php
@@ -10,45 +10,46 @@
 					    	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					    	<?php the_content(); endwhile; endif; wp_reset_postdata(); ?>
 					    </div>
-				    	<?php 
-				    	$args = array(
-				    		'posts_per_page' => 8,
-				    		'tax_query' => array(
-						        array(
-						            'taxonomy' => 'post_format',
-						            'field' => 'slug',
-						            'terms' => array(
-						            	'post-format-aside',
-						            	'post-format-gallery',
-						            	'post-format-audio',
-						            	'post-format-image'
-						            ),
-						            'operator' => 'NOT IN'
-						        )
-						    )
-							
-						); ?>
-				    	<?php $my_query = new WP_Query( $args );  $c = 0; ?>
-					    <?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); 
-					    	 
-					    	if ($c == 0 || $c % 2 == 0) {
-					    		$style = 'first ';
-					    		$c = 0;
-					    	} else {
-					    		$style = '';
-					    	}
-					    	$c++;
-					    ?>
-						<div class="<?php echo $style; ?>threecol">
-						        <h2 class="h1"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					    <div class="sixcol">
+					    	<?php 
+					    	$args = array(
+					    		'posts_per_page' => 8,
+					    		'tax_query' => array(
+							        array(
+							            'taxonomy' => 'post_format',
+							            'field' => 'slug',
+							            'terms' => array(
+							            	'post-format-aside',
+							            	'post-format-gallery',
+							            	'post-format-audio',
+							            	'post-format-image'
+							            ),
+							            'operator' => 'NOT IN'
+							        )
+							    )
+								
+							); ?>
+					    	<?php $my_query = new WP_Query( $args );  $c = 0; ?>
+						    <?php if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); 
+						    	 
+						    	if ($c == 0 || $c % 2 == 0) {
+						    		$style = 'first ';
+						    		$c = 0;
+						    	} else {
+						    		$style = '';
+						    	}
+						    	$c++;
+						    ?>
+							<div class="<?php echo $style; ?>sixcol">
+							        <h2 class="h1"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							    
+							    <?php the_excerpt(); ?>
+							</div>
+						    <?php endwhile; ?>		
+						
 						    
-						    <?php the_excerpt(); ?>
-						</div>
-					    <?php endwhile; ?>		
-					
-					    
-					    <?php endif; wp_reset_postdata(); ?>
-					    
+						    <?php endif; wp_reset_postdata(); ?>
+					    </div>
 					    
 			
     				</div> <!-- end #main -->
