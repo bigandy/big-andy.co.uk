9ea2ffbfced91d461234e907c7d8639359a4b55f
Andrew Hudson <ahudson@gmail.com>
2012-10-15 14:53:32 +0100

    remove plugin scripts and add them to one script.min.js file

--- content/themes/bones/library/bones.php
+++ content/themes/bones/library/bones.php
@@ -114,9 +114,9 @@ function bones_scripts_and_styles() {
   if (!is_admin()) {
   
     // modernizr (without media query polyfill)
-    wp_register_script( 'bones-modernizr', get_stylesheet_directory_uri() . '/library/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
+    // wp_register_script( 'bones-modernizr', get_stylesheet_directory_uri() . '/library/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
 
-    wp_register_script( 'bones-fitvids', get_stylesheet_directory_uri() . '/library/js/libs/jquery.fitvids.js', array(), '1.0', false );
+    // wp_register_script( 'bones-fitvids', get_stylesheet_directory_uri() . '/library/js/libs/jquery.fitvids.js', array(), '1.0', false );
  
     // register main stylesheet
     wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
@@ -130,7 +130,7 @@ function bones_scripts_and_styles() {
     }
     
     // adding scripts file in the footer
-    wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
+    wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.min.js', array( 'jquery' ), '', true );
     
     // enqueue styles and scripts
     wp_enqueue_script( 'bones-modernizr' ); 
