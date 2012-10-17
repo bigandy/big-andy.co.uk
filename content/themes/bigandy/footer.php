06fd196f018f3e224acadeecbae32b0373708b1d
Andrew Hudson <ahudson@gmail.com>
2012-10-17 11:39:17 +0100

    remove un-used themes

--- content/themes/bigandy/footer.php
+++
@@ -1,9 +0,0 @@
-<footer id="footer">
-	<!--END-->
-	<p>&copy;2004-<?php echo date('Y');?> Andrew JD Hudson</p>
-	<p>Powered by <a href="http://wordpress.org" target="_blank" title="link opens in new window - WordPress">WordPress</a></p>
-</footer><!--end of footer-->
-</div><!--end of container-->
-<?php wp_footer(); ?>
-</body>
-</html>
