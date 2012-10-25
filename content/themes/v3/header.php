7e1b61b0e56e553075530c2138be66efea91e127
Andrew Hudson <ahudson@gmail.com>
2012-10-25 20:37:21 +0100

    add enquire.js

--- content/themes/v3/header.php
+++ content/themes/v3/header.php
@@ -43,11 +43,6 @@
 	
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">				
 	<?php wp_head(); ?>
-	<script>
-	jQuery(document).ready(function($) {
-		$('body').addClass('js');
-	}); 
-	</script>
 </head>
 	
 <body>
