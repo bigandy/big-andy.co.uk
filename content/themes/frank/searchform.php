21ca990b5de3d204608e88c722352d416e131a5f
Andrew Hudson <ahudson@gmail.com>
2012-10-25 21:11:36 +0100

    remove frank theme

--- content/themes/frank/searchform.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package WordPress
- * @subpackage Frank
- */
-?>
-<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-	<input type="text" placeholder="Type your query, hit enter" class='textinput' name="s" id="s" />
-</form>
