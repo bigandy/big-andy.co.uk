d211f4905eee4d2b23e65d849073b685527a9b99
Andrew Hudson <ahudson@gmail.com>
2012-10-31 21:22:40 +0000

    remove meta info. make css/js work on aws

--- content/themes/v3/footer.php
+++ content/themes/v3/footer.php
@@ -15,7 +15,7 @@
 		</div> <!-- end .container -->
 		
 		<?php wp_footer(); // js scripts are inserted using this function ?>
-		<script type="text/javascript" src="/content/themes/v3/js/scripts.min.js"></script>
+		<script type="text/javascript" src="/wordpress/wp-content/themes/v3/js/scripts.min.js"></script>
 
 	</body>
 
