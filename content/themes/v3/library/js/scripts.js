6ee938678e4fecdaa5ec31e72f1ee723a2b1bed2
Andrew Hudson <ahudson@gmail.com>
2012-11-15 20:44:03 +0000

    remove typekit js from header + into main js file

--- content/themes/v3/library/js/scripts.js
+++ content/themes/v3/library/js/scripts.js
@@ -1,3 +1,10 @@
+  (function() {
+    var config = {
+      kitId: 'bhs0nmm',
+      scriptTimeout: 3000
+    };
+    var h=document.getElementsByTagName("html")[0];h.className+=" js wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+  })();
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-6954334-3']);
   _gaq.push(['_trackPageview']);
