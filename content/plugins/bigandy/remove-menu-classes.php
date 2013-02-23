e3285cb27b222d0d929e987878900d68e570e846
Andrew Hudson <ahudson@gmail.com>
2013-02-23 21:46:17 +0000

    remove menu class current_page_parent from blog for custom post types, add back class to body

--- content/plugins/bigandy/remove-menu-classes.php
+++ content/plugins/bigandy/remove-menu-classes.php
@@ -7,3 +7,27 @@ add_filter( 'nav_menu_css_class', 'my_css_attributes_filter', 100, 1 );
 function my_css_attributes_filter( $var ) {
 	return is_array( $var ) ? array_intersect( $var, array( 'current-menu-item', 'current_page_parent' ) ) : '';
 }
+
+
+// http://vayu.dk/highlighting-wp_nav_menu-ancestor-children-custom-post-types/
+// on custom post-types removes class current_page_parent from blog
+
+add_filter('nav_menu_css_class', 'current_type_nav_class', 10, 2);
+function current_type_nav_class($classes, $item) {
+    // Get post_type for this post
+    $post_type = get_query_var('post_type');
+
+    // Removes current_page_parent class from blog menu item
+    if ( get_post_type() == $post_type )
+        $classes = array_filter($classes, "get_current_value" );
+
+    // Go to Menus and add a menu class named: {custom-post-type}-menu
+    // This adds a current_page_parent class to the parent menu item
+    if( in_array( $post_type.'-menu-item', $classes ) )
+        array_push($classes, 'current_page_parent');
+
+    return $classes;
+}
+function get_current_value( $element ) {
+    return ( $element != "current_page_parent" );
+}
--- content/themes/v3/header.php
+++ content/themes/v3/header.php
@@ -22,7 +22,7 @@
 	<?php wp_head(); ?>
 </head>
 	
-<body>
+<body <?php body_class(); ?>>
 	
 	<div class="container">
 		<header class="header" role="banner">
