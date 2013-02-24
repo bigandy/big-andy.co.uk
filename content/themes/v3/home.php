caee31f814f5907ffbd83f496f1f3053a72fd483
Andrew Hudson <ahudson@gmail.com>
2013-02-24 13:36:07 +0000

    sticky post first loop, everything except for first sticky in second loop improve code formatting

--- content/themes/v3/home.php
+++ content/themes/v3/home.php
@@ -10,8 +10,9 @@
 
 	    	<?php
 	    	// get id of category 'hide'
-	    	$idObj = get_category_by_slug( 'hide' );
-		    $hide_id = $idObj->term_id;
+	    	// $idObj = get_category_by_slug( 'hide' );
+		    // $hide_id = $idObj->term_id;
+		    $hide_id = 31;
 		    // get paged
 	    	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 
@@ -20,9 +21,20 @@
 	    		if ( $paged < 2 )
 
 				{
+					$sticky = get_option( 'sticky_posts' );
+
+					/* Sort the stickies with the newest ones at the top */
+					rsort( $sticky );
+
+		            /* Get the 5 newest stickies (change 5 for a different number) */
+		            $sticky = array_slice( $sticky, 0, 1 );
+
 					$first_article_loop_args = array(
-						'cat' => $hide_id,
+						'cat' => -$hide_id,
 						'posts_per_page' => 1,
+						'post__in' => $sticky,
+		                'ignore_sticky_posts' => 1,
+		                'paged' => $paged
 					);
 
 			    	$first_article_loop = new WP_Query( $first_article_loop_args );
@@ -31,7 +43,7 @@
 						while ( $first_article_loop->have_posts() ) :
 							$first_article_loop->the_post(); ?>
 
-							<article <?php post_class('clearfix'); ?> role="article">
+							<article role="article">
 								<h2>
 									<a href="<?php the_permalink(); ?>" rel="bookmark">
 										<?php the_title(); ?>
@@ -42,7 +54,7 @@
 						<?php
 						endwhile;
 					endif;
-				wp_reset_postdata(); // end of first_article_loop
+					wp_reset_postdata(); // end of first_article_loop
 				} // end if $paged > 2
 
 			} // end of current_user_can('update-core')
@@ -50,21 +62,19 @@
 
 			$non_hide_loop_args = array(
 				'cat' => -$hide_id,
-				'paged' => $paged
+				'post__not_in' => $sticky,
+                'ignore_sticky_posts' => 1,
+                'paged' => $paged
 			);
 
 			$non_hide_loop = new WP_Query( $non_hide_loop_args );
 		    if ( $non_hide_loop->have_posts() ) :
-		    		while ($non_hide_loop->have_posts())
-		    			: $non_hide_loop->the_post(); ?>
+		    		while ( $non_hide_loop->have_posts() ) :
+		    			$non_hide_loop->the_post(); ?>
 
 					    <article <?php post_class('clearfix'); ?> role="article">
 
-					    	<?php
-					    		if ( is_sticky() ) {
-					    			echo "sticky";
-					    		}
-					    	?>
+
 
 							<?php if( !has_post_format( 'aside' ) ){ ?>
 								<header class="article-header">
@@ -98,15 +108,8 @@
 
 		        <?php if (function_exists('bones_page_navi')) { // if experimental feature is active ?>
 
-			        <?php // bones_page_navi(); // use the page navi function ?>
+			        <?php bones_page_navi(); // use the page navi function ?>
 
-		        <?php } else { // if it is disabled, display regular wp prev & next links ?>
-			        <!-- <nav class="wp-prev-next">
-				        <ul class="clearfix">
-					        <li class="prev-link"><?php next_posts_link(_e('&laquo; Older Entries', 'bonestheme')) ?></li>
-					        <li class="next-link"><?php previous_posts_link(_e('Newer Entries &raquo;', 'bonestheme')) ?></li>
-				        </ul>
-			        </nav> -->
 		        <?php } ?>
 
 		    <?php endif; wp_reset_postdata(); ?>
