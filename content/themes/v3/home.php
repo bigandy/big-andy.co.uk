561bb41e49a278f89aa42ce4592de41dc130ab23
Andrew Hudson <ahudson@gmail.com>
2013-02-24 14:44:54 +0000

    remove if current_user_can and streamline other stuff

--- content/themes/v3/home.php
+++ content/themes/v3/home.php
@@ -6,58 +6,52 @@
 
 	    <div class="eightcol first clearfix main" role="main">
 
-	    	<!-- Hidden Post Class -->
-
 	    	<?php
-	    	// get id of category 'hide'
-	    	// $idObj = get_category_by_slug( 'hide' );
-		    // $hide_id = $idObj->term_id;
+	    	// hidden post class
 		    $hide_id = 31;
 		    // get paged
 	    	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 
-	    	if( current_user_can( 'update_core') ) {
+    		if ( $paged < 2 )
 
-	    		if ( $paged < 2 )
+			{
+				$sticky = get_option( 'sticky_posts' );
 
-				{
-					$sticky = get_option( 'sticky_posts' );
+				/* Sort the stickies with the newest ones at the top */
+				rsort( $sticky );
 
-					/* Sort the stickies with the newest ones at the top */
-					rsort( $sticky );
+	            /* Get the 1 newest stickies (change 1 for a different number) */
+	            $sticky = array_slice( $sticky, 0, 1 );
 
-		            /* Get the 5 newest stickies (change 5 for a different number) */
-		            $sticky = array_slice( $sticky, 0, 1 );
+				$sticky_loop_args = array(
+					'cat' => -$hide_id,
+					'posts_per_page' => 1,
+					'post__in' => $sticky,
+	                'ignore_sticky_posts' => 1,
+				);
 
-					$first_article_loop_args = array(
-						'cat' => -$hide_id,
-						'posts_per_page' => 1,
-						'post__in' => $sticky,
-		                'ignore_sticky_posts' => 1,
-		                'paged' => $paged
-					);
+		    	$sticky_loop = new WP_Query( $sticky_loop_args );
 
-			    	$first_article_loop = new WP_Query( $first_article_loop_args );
+				if( $sticky_loop->have_posts() ) :
+					while ( $sticky_loop->have_posts() ) :
+						$sticky_loop->the_post(); ?>
 
-					if( $first_article_loop->have_posts() ) :
-						while ( $first_article_loop->have_posts() ) :
-							$first_article_loop->the_post(); ?>
+						<article role="article">
+							<h2>
+								<a href="<?php the_permalink(); ?>" rel="bookmark">
+									<?php the_title(); ?>
+								</a>
+							</h2>
+							<section class="post-content clearfix">
+							    <?php the_content(); ?>
+						    </section> <!-- end article section -->
+						</article>
 
-							<article role="article">
-								<h2>
-									<a href="<?php the_permalink(); ?>" rel="bookmark">
-										<?php the_title(); ?>
-									</a>
-								</h2>
-							</article>
-
-						<?php
-						endwhile;
-					endif;
-					wp_reset_postdata(); // end of first_article_loop
-				} // end if $paged > 2
-
-			} // end of current_user_can('update-core')
+					<?php
+					endwhile;
+				endif;
+				wp_reset_postdata(); // end of sticky_loop
+			} // end if $paged > 2
 
 
 			$non_hide_loop_args = array(
@@ -102,7 +96,8 @@
 					    </article> <!-- end article -->
 
 	    		<?php
-	    	 		endwhile; // end non_hide_loop()
+	    	 		endwhile;
+	    	 		wp_reset_postdata(); // end non_hide_loop()
 	    	 	?>
 
 
@@ -112,7 +107,7 @@
 
 		        <?php } ?>
 
-		    <?php endif; wp_reset_postdata(); ?>
+		    <?php endif; ?>
 
 	    </div> <!-- end .main -->
 
