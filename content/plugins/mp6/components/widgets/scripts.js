d3eafc8d86b7f74e091a448e7b25de19a709f9ee
Andrew Hudson <ahudson@gmail.com>
2013-11-17 20:14:21 +0000

    update plugins

--- content/plugins/mp6/components/widgets/scripts.js
+++ content/plugins/mp6/components/widgets/scripts.js
@@ -1,4 +1,4 @@
-;var widgets = ( function( $, window, document, undefined ) {
+( function( $, window, document, undefined ) {
 
 	$(document).ready( function() {
 		$( '#widgets-right .widgets-sortables .sidebar-description' ).each( function(index, element) {
@@ -9,12 +9,13 @@
 		$( '#available-widgets .widget-top' ).append( '<div class="more-info"></div>' );
 
 		$( '#available-widgets .more-info' ).on( 'mouseenter', function() {
-			var description = $( this ).closest( '.widget' ).find('.widget-description'),
+			var $this = $( this ),
+				description = $this.closest( '.widget' ).find('.widget-description'),
 				description_timer = setTimeout( function() {
 					description.fadeIn(200);
 				}, 400 );
-			$( this ).on( 'mouseleave', function() {
-				clearInterval( description_timer );
+			$this.on( 'mouseleave', function() {
+				clearTimeout( description_timer );
 				description.fadeOut(200);
 			} );
 		} );
