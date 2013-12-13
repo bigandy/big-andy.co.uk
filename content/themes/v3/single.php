cce73ba582d5a84779494954391c373c99f6763d
Andrew Hudson <ahudson@gmail.com>
2013-12-13 13:50:15 +0000

    fix broken single.php

--- content/themes/v3/single.php
+++ content/themes/v3/single.php
@@ -31,21 +31,8 @@
 									} // comments should go inside the article element ?>
 								</article>
 							<?php
-							} else {
-							?>
-								<article class="hentry clearfix post-not-found">
-						    		<header class="article-header">
-						    			<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
-						    		</header>
-						    		<section class="post-content">
-						    			<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-						    		</section>
-						    		<footer class="article-footer">
-						    		    <p><?php _e("This is the error message in the single.php template.", "bonestheme"); ?></p>
-						    		</footer>
-								</article>
-							<?php
 							}
+
 						} ?>
 					</div>
 				</div>
