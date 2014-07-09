66fb140d4fc209fe1c40929b1c6d06a0c9f483d8
Andrew Hudson <ahudson@gmail.com>
2014-07-09 14:13:19 +0000

    update footer and page to have same alignment as front-page

--- content/themes/v4/footer.php
+++ content/themes/v4/footer.php
@@ -1,6 +1,6 @@
 		<footer>
 			<div class="row footer__row content-container">
-				<div class="large-12 columns">
+				<div class="large-8 large-push-2 small-12 columns">
 					<p>&copy; <?php echo date('Y'); ?> Andrew JD Hudson</p>
 				</div>
 			</div>
