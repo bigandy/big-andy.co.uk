01939eed5c9b1f793f0354b242c59b3a01e32367
Andrew Hudson <ahudson@gmail.com>
2014-10-10 16:40:51 +0100

    add flex video support

--- content/themes/v4/functions/misc.php
+++ content/themes/v4/functions/misc.php
@@ -24,3 +24,15 @@ function ah_excerpt_more( $more ) {
 	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
 }
 add_filter( 'excerpt_more', 'ah_excerpt_more' );
+
+/*
+ * function es_wrap_embed()
+ * Wrap embed in div.flex-video
+ * http://wordpress.stackexchange.com/questions/119547/oembed-youtube-video-aspect-ratio
+ */
+if ( ! function_exists( 'ah_wrap_embed' ) ) {
+	function ah_wrap_embed( $html, $url, $attr, $post_id ) {
+		return '<div class="flex-video">' . $html . '</div>';;
+	}
+	add_filter( 'embed_oembed_html', 'ah_wrap_embed', 10, 4 );
+}
