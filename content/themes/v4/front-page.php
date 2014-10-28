f6ad6332786afa87d9916d7f8fd334c146b983b1
Andrew Hudson <ahudson@gmail.com>
2014-10-28 10:59:46 +0000

    Revert "trying out some different design ideas"
    This reverts commit 941334fbe7631b5a60929eecbbcb4618889943e7.

--- content/themes/v4/front-page.php
+++ content/themes/v4/front-page.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
-<main class="row content-container content-container--home">
+<main class="row content-container">
+	<section class="large-10 large-push-1 small-12 columns home__intro">
+		<?php
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				?>
+				<h2 class="home__intro">
+					<?php echo get_the_content(); ?>
+				</h2>
+				<?php
+			}
+		}
+		?>
+    </section>
     <?php
 	// hidden post
 	$hide_id = 31;
@@ -12,35 +26,26 @@
 	$home_loop = new WP_Query( $home_args );
 
 	if ( $home_loop->have_posts() ) {
-		?>
-		<div class="large-push-1 large-10 columns">
-		<?php
 		while ( $home_loop->have_posts() ) {
 			$home_loop->the_post();
 			?>
-			<article role="article" class="">
-
-
+			<article role="article" class="large-8 large-push-2 small-12 columns">
 				<header class="article__header article__header--front-page">
-					<time class="article__time" datetime="<?php the_time( 'c' ); ?>">
-						<?php the_time( 'jS F Y' ); ?>
-					</time>
 					<h2 class="article__title">
 						<a href="<?php the_permalink(); ?>">
 							<?php the_title(); ?>
 						</a>
 					</h2>
+					<time class="article__time" datetime="<?php the_time( 'c' ); ?>">
+						<?php the_time( 'jS F Y' ); ?>
+					</time>
 				</header>
-
 				<div class="post-content clearfix article__content--front-page">
 					<?php the_excerpt(); ?>
 				</div>
 			</article>
 			<?php
 		}
-		?>
-		</div>
-		<?php
 	}
 	wp_reset_postdata();
 	?>
