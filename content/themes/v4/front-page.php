3197bd086d2bf3f482f9c73384db1435752acae5
Andrew Hudson <ahudson@gmail.com>
2014-10-28 16:19:43 +0000

    change date format, more dotted lines

--- content/themes/v4/front-page.php
+++ content/themes/v4/front-page.php
@@ -1,55 +1,58 @@
 <?php get_header(); ?>
 <main class="row content-container">
 	<div class="large-8 large-push-2 small-12 columns">
-			<section class="home__intro">
-				<?php
-				if ( have_posts() ) {
-					while ( have_posts() ) {
-						the_post();
-						?>
-						<h2 class="home__intro">
-							<?php echo get_the_content(); ?>
-						</h2>
-						<?php
-					}
-				}
-				?>
-		    </section>
-		    <?php
-			// hidden post
-			$hide_id = 31;
-
-			$home_args = array(
-				'posts_per_page' => 6,
-				'cat' => -$hide_id,
-			);
-
-			$home_loop = new WP_Query( $home_args );
-
-			if ( $home_loop->have_posts() ) {
-				while ( $home_loop->have_posts() ) {
-					$home_loop->the_post();
+		<section class="home__intro">
+			<?php
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
 					?>
-					<article role="article" class="article--home">
-						<header class="article__header article__header--front-page">
-							<h2 class="article__title">
-								<a href="<?php the_permalink(); ?>">
-									<?php the_title(); ?>
-								</a>
-							</h2>
-							<time class="article__time" datetime="<?php the_time( 'c' ); ?>">
-								<?php the_time( 'jS F Y' ); ?>
-							</time>
-						</header>
-						<div class="post-content clearfix article__content--front-page">
-							<?php the_excerpt(); ?>
-						</div>
-					</article>
+					<h2 class="home__intro">
+						<?php echo get_the_content(); ?>
+					</h2>
 					<?php
 				}
 			}
-			wp_reset_postdata();
 			?>
+	    </section>
+	    <section class="home__articles">
+    	    <?php
+    		// hidden post
+    		$hide_id = 31;
+
+    		$home_args = array(
+    			'posts_per_page' => 6,
+    			'cat' => -$hide_id,
+    		);
+
+    		$home_loop = new WP_Query( $home_args );
+
+    		if ( $home_loop->have_posts() ) {
+    			while ( $home_loop->have_posts() ) {
+    				$home_loop->the_post();
+    				?>
+    				<article role="article" class="article--home">
+    					<header class="article__header article__header--front-page">
+
+    						<h2 class="article__title">
+    							<a href="<?php the_permalink(); ?>">
+    								<?php the_title(); ?>
+    							</a>
+    						</h2>
+							<time class="article__time" datetime="<?php the_time( 'c' ); ?>">
+								<?php the_time( 'd/m/Y' ); ?>
+							</time>
+    					</header>
+    					<div class="post-content clearfix article__content--front-page">
+    						<?php the_excerpt(); ?>
+    					</div>
+    				</article>
+    				<?php
+    			}
+    		}
+    		wp_reset_postdata();
+    		?>
+	    </section>
 	</div>
 
 </main>
