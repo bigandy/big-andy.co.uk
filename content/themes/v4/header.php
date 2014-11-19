111a73bd2a0b143f61cb7af9067368faf01f7f1f
Andrew Hudson <ahudson@gmail.com>
2014-11-19 11:32:10 +0000

    add constants, add 192x192 icon

--- content/themes/v4/header.php
+++ content/themes/v4/header.php
@@ -7,6 +7,7 @@
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta name="theme-color" content="rgba(0,0,0,0.25)">
+	<link rel="icon" sizes="192x192" href="<?php echo TEMPLATEURI; ?>images/ba-octagon.svg">
 	<style><?php include_once( 'style.css' ); ?></style>
 	<title>
 		<?php wp_title(); ?>
