a08f906a7a5c7059dccdd954a4e138b5e8d7416c
Andrew Hudson <ahudson@gmail.com>
2015-01-30 09:46:05 +0000

    rename filename to reflect that its not using the picture element anymore

--- content/themes/v4/functions.php
+++ content/themes/v4/functions.php
@@ -4,7 +4,7 @@ $functions = get_template_directory() . '/functions/';
 include( $functions . 'misc.php' );
 include( $functions . 'ss.php' );
 include( $functions . 'images.php' );
-include( $functions . 'picture.php' );
+include( $functions . 'responsive-images.php' );
 include( $functions . 'nav.php' );
 include( $functions . 'shortcodes.php' );
 include( $functions . 'customizer.php' );
