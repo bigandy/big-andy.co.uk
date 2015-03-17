4db375910acdf0b807768d60df9b39665a0a35a8
Andrew Hudson <ahudson@gmail.com>
2015-03-17 10:13:23 +0000

    ignore bower_components, do some different on homepage with bg images'

--- content/themes/v5/front-page.php
+++ content/themes/v5/front-page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <main class="row content-container">
 	<div class="large-12 small-12 columns">
-	    <section class="home__articles flex-container">
-    	    <?php
+		<section class="home__articles flex-container">
+			<?php
 			// hidden post
 			$hide_id = 31;
 
@@ -16,8 +16,21 @@
 			if ( $home_loop->have_posts() ) {
 				while ( $home_loop->have_posts() ) {
 					$home_loop->the_post();
+
+					if ( has_post_thumbnail() ) {
+						$post_thumbnail_id = get_post_thumbnail_id();
+						$thumb_src = 'style="background-image: url(' . wp_get_attachment_image_src( $post_thumbnail_id, 'pic-small' )[0] . ');"';
+						$class = 'article--home--image';
+
+						// ah_preit( $thumb_src );
+					} else {
+						$thumb_src = '';
+						$class = '';
+					}
+
 					?>
-					<article role="article" class="article--home flex-item">
+					<article role="article" class="article--home flex-item <?php echo $class; ?>" <?php echo $thumb_src; ?>>
+
 						<header class="article__header article__header--front-page">
 							<h2 class="article__title">
 								<a href="<?php the_permalink(); ?>">
@@ -28,9 +41,9 @@
 								<?php the_time( 'd/m/Y' ); ?>
 							</time>
 						</header>
-						<div class="post-content clearfix article__content--front-page">
+						<!-- <div class="post-content clearfix article__content--front-page">
 							<?php the_excerpt(); ?>
-						</div>
+						</div> -->
 					</article>
 					<?php
 				}
@@ -38,7 +51,7 @@
 
 			wp_reset_postdata();
 			?>
-	    </section>
+		</section>
 	</div>
 
 </main>
