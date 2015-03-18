009f12e8c19fcbbdf42882cf7efec075c62c5a9f
Andrew Hudson <ahudson@gmail.com>
2015-03-18 16:57:17 +0000

    add initial mainfest file, update plugin

--- content/themes/v4/header.php
+++ content/themes/v4/header.php
@@ -6,6 +6,8 @@
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<!-- Startup configuration -->
+	<link rel="manifest" href="<?php echo esc_url( TEMPLATEURI ); ?>manifest.json">
 	<link rel="icon" sizes="192x192" href="<?php echo esc_url( TEMPLATEURI ); ?>images/ba3.png" type="image/svg+xml">
 	<style><?php
 	if ( is_admin() ) {
