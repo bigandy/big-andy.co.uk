9be04eeafbeb0ec92d475b0d44c74da1bb54a3d9
Andrew Hudson <ahudson@gmail.com>
2015-04-07 17:31:07 +0100

    remove emojis css

--- content/themes/v4/functions/ss.php
+++ content/themes/v4/functions/ss.php
@@ -10,3 +10,9 @@ function ah_enqueue_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'ah_enqueue_scripts' );
+
+function ah_remove_emojis() {
+	// remove emoji styles
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+}
+add_action( 'init', 'ah_remove_emojis' );
