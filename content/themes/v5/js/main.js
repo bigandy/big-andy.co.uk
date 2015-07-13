71d98cfe8e64c77ca68b120d560ec6a925d04ac3
Andrew Hudson <ahudson@gmail.com>
2015-07-13 15:02:00 +0100

    add weather checker to homepage banner

--- content/themes/v5/js/main.js
+++ content/themes/v5/js/main.js
@@ -1,21 +1,47 @@
 /* global localStorage, window, document */
 
-// http://joelcalifa.com/blog/revisiting-visited
-localStorage.setItem('visited-' + window.location.pathname, true);
-
-var links = document.getElementsByClassName('article__link'),
-	linksLength = links.length,
-	i = linksLength;
+(function ($) {
+	'use strict';
 
 
-while (i--) {
-	var link = links[i];
-	if (link.host === window.location.host && localStorage.getItem('visited-' + link.pathname)) {
 
-		// there's got to be a better way of getting the containing article
-		link.parentNode.parentNode.parentNode.dataset.visited = true;
+	var links = document.getElementsByClassName('article__link'),
+		linksLength = links.length,
+		i = linksLength,
+		homeWeather = $('.home__weather');
+
+
+	// http://joelcalifa.com/blog/revisiting-visited
+	localStorage.setItem('visited-' + window.location.pathname, true);
+
+	while (i--) {
+		var link = links[i];
+		if (link.host === window.location.host && localStorage.getItem('visited-' + link.pathname)) {
+
+			// there's got to be a better way of getting the containing article
+			link.parentNode.parentNode.parentNode.dataset.visited = true;
+		}
 	}
-}
 
 
 
+	function getWeather(data) {
+		// using units=metric for celcius - can be changed
+		var weather = '//api.openweathermap.org/data/2.5/weather?q=oxford,uk&units=metric&APPID=d8c7ee9216b8e2039669c7b012fcc66e';
+
+		$.ajax({
+			dataType: 'jsonp',
+			url: weather,
+			success: function (data) {
+				console.log(data);
+
+				console.log('weather is ' + data.weather[0].main);
+				homeWeather.text(data.weather[0].main + 'y');
+
+			}
+		});
+	}
+
+	getWeather();
+
+}(jQuery));
