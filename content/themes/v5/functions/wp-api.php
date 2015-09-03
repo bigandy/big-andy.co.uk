f79d0e321ecaa846938c1b6ba09b7323ad28c3c6
Andrew Hudson <ahudson@gmail.com>
2015-09-03 16:51:55 +0100

    output all image sizes

--- content/themes/v5/functions/wp-api.php
+++ content/themes/v5/functions/wp-api.php
@@ -1,10 +1,21 @@
 <?php
 function ah_rest_prepare_post( $data, $post, $request ) {
+	global $_wp_additional_image_sizes;
+
+	if ( empty( $_wp_additional_image_sizes ) ) {
+		return $sizes;
+	}
 	$_data = $data->data;
 	$thumbnail_id = get_post_thumbnail_id( $post->ID );
-	$thumbnail = wp_get_attachment_image_src( $thumbnail_id, 'full' );
 
-	$_data['featured_image_full_url'] = $thumbnail[0];
+	$featured_image_urls = [];
+	foreach ( $_wp_additional_image_sizes as $size => $value ) {
+		$thumbnail_info = wp_get_attachment_image_src( $thumbnail_id, $size );
+
+		$featured_image_urls[$size] = $thumbnail_info[0];
+	}
+
+	$_data['featured_image_full_urls'] = $featured_image_urls;
 	$data->data = $_data;
 	return $data;
 }
