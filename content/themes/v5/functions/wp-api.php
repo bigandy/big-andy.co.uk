4d0f19272705ae6a8370d90434f014b4738f36df
Andrew Hudson <ahudson@gmail.com>
2015-09-21 09:04:39 +0100

    remove example permissions code

--- content/themes/v5/functions/wp-api.php
+++ content/themes/v5/functions/wp-api.php
@@ -21,29 +21,4 @@ function ah_rest_prepare_post( $data, $post, $request ) {
 }
 add_filter( 'rest_prepare_post', 'ah_rest_prepare_post', 10, 3 );
 
-function ah_title_from_id( $data ) {
-	$posts = get_posts( array(
-		'author' => $data['id'],
-	) );
 
-	if ( empty( $posts ) ) {
-		return new WP_Error( 'no_author', 'Invalid author', array( 'status' => 404 ) );
-	}
-
-	return $posts[0]->post_title;
-}
-
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'ah/v1', '/author/(?P<id>.+)', array(
-		'methods' => 'GET',
-		'callback' => 'ah_title_from_id',
-		'args' => array(
-			'id' => array(
-				'validate_callback' => function ( $val ) { return is_numeric( $val ); },
-			),
-		),
-		'permission_callback' => function () {
-			return current_user_can( 'edit_others_posts' );
-		}
-	) );
-} );
