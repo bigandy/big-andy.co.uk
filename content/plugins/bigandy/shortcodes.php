9d1d15680d2fe788a34d2d27a189ffa4990e3760
Andrew Hudson <ahudson@gmail.com>
2015-10-22 15:52:54 +0100

    update plugin

--- content/plugins/bigandy/shortcodes.php
+++ content/plugins/bigandy/shortcodes.php
@@ -84,3 +84,103 @@ function ah_vcard_shortcode( $atts, $content = null ) {
 	return '<div class="vcard">'. do_shortcode( $content ) .'</div>';
 }
 add_shortcode( 'vcard', 'ah_vcard_shortcode' );
+
+function ah_empty_paragraph_fix( $content ) {
+	// An array of the offending tags.
+	$arr = array(
+		'<p>[' => '[',
+		']</p>' => ']',
+		']<br />' => ']',
+	);
+	// Remove the offending tags and return the stripped content.
+	$stripped_content = strtr( $content, $arr );
+	return $stripped_content;
+}
+add_filter( 'the_content', 'ah_empty_paragraph_fix' );
+
+/**
+ * Shorcode to produce half column
+ */
+function ah_shortcode_half( $atts, $content ) {
+	$html = '<div class="large-6 columns">' . do_shortcode( $content ) . '</div>';
+	return $html;
+}
+add_shortcode( 'half', 'ah_shortcode_half' );
+
+function ah_shortcode_full( $atts, $content ) {
+	$html = '<div class="large-12 columns">' . do_shortcode( $content ) . '</div>';
+	return $html;
+}
+add_shortcode( 'full', 'ah_shortcode_full' );
+
+function ah_shortcode_row( $atts, $content ) {
+	$html = '<div class="row">' . do_shortcode( $content ) . '</div>';
+	return $html;
+}
+add_shortcode( 'row', 'ah_shortcode_row' );
+
+function ah_shortcode_icon( $atts ) {
+	$atts = shortcode_atts( array(
+		'type' => 'pen',
+		'size' => '',
+	), $atts, 'icon' );
+
+	$size = '';
+	$type = $atts['type'];
+
+	if ( $atts['size'] !== '' ) {
+		$size = ' icon--' . $atts['size'];
+	}
+
+	$html = '<svg class="icon icon--shortcode' . $size . '"><use xlink:href="#'. $type .'" /></svg>';
+	return $html;
+}
+add_shortcode( 'icon', 'ah_shortcode_icon' );
+
+function ah_shortcode_lazy( $atts, $content ) {
+
+	$html = '<div data-lazy-widget="lazy" class="g-plusone"></div>';
+
+	$html .= '<div id="lazy"><!--';
+		$html .= $content;
+	$html .= '--></div>';
+
+	return $html;
+}
+add_shortcode( 'lazy', 'ah_shortcode_lazy' );
+
+function ah_shortcode_weather( $atts, $content ) {
+	$response_args = [
+		'timeout' => 120,
+	];
+
+	// delete_transient( 'weather' ); // if you want to delete the transient, uncomment this line
+
+	$transient_name = 'weather'; // Name of value in database.
+	$cache_time = 3; // Time in hours between updates.
+
+	// If the transient is not set
+	if ( false === ( $weather = get_transient( $transient_name ) ) ) {
+	   // Get new $response from the weather api
+	   $response = wp_remote_get( 'https://api.forecast.io/forecast/c3a6795997c7501f0e1e115b3600eb40/51.628284,-1.296668?units=uk', $response_args );
+	   // Get response body
+	   $response_body = json_decode( $response['body'] );
+	   // Get Current Weather Summary
+	   $weather = strtolower( $response_body->currently->summary );
+	   // Set the transient
+	   set_transient( $transient_name, $weather, 60 * 60 * $cache_time );
+	}
+
+	$html = $weather;
+
+	// if ( is_array( $response ) ) {
+
+	// 	$response_body = json_decode( $response['body'] );
+	// 	$html = strtolower( $response_body->currently->summary );
+	// } else {
+	// 	$html = 'sunny';
+	// }
+
+	return esc_html( $html );
+}
+add_shortcode( 'weather', 'ah_shortcode_weather' );
