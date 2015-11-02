02140ba3c2aba95181acfd46cda21f2bb0b5c9c9
Andrew Hudson <ahudson@gmail.com>
2015-11-02 16:32:02 +0000

    update to latest WordPress and WP-API from Github

--- content/plugins/bigandy/security-stuff.php
+++ content/plugins/bigandy/security-stuff.php
@@ -7,10 +7,10 @@ remove_action( 'wp_head', 'wp_generator' );
 /* remove version numbers from end of css and js files
 * http://wordpress.org/support/topic/get-rid-of-ver-on-the-end-of-cssjs-files#post-1892166
 */
-function remove_cssjs_ver( $src ) {
+function ah_remove_cssjs_ver( $src ) {
 	if ( strpos( $src, '?ver=' ) )
 		$src = remove_query_arg( 'ver', $src );
 	return $src;
 }
-add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
-add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'style_loader_src', 'ah_remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'ah_remove_cssjs_ver', 10, 2 );
