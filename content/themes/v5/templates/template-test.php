610ccccf24df3b27f99c7492d2b82478b549f5f7
Andrew Hudson <ahudson@gmail.com>
2015-12-09 09:29:16 +0000

    update template to include content as well

--- content/themes/v5/templates/template-test.php
+++ content/themes/v5/templates/template-test.php
@@ -3,10 +3,33 @@ get_header();
 /*
  * Template Name: Test Remote Get
  */
-$api_url = 'http://api.wordpress.org/secret-key/1.0/';
-$response = wp_remote_get( $api_url );
-$header = wp_remote_retrieve_headers( $response );
+?>
+<main>
+	<div class="row content-container">
+		<?php
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				?>
+				<article role="article" class="large-12 columns">
+					<section class="post-content clearfix row">
+					<?php
+					$api_url = 'http://api.wordpress.org/secret-key/1.0/';
+					$response = wp_remote_get( $api_url );
+					$header = wp_remote_retrieve_headers( $response );
 
-var_dump( $header );
+					var_dump( $header );
 
+					the_content();
+					?>
+				</section>
+			</article>
+			<?php
+			}
+		}
+		wp_reset_postdata();
+		?>
+	</div>
+</main>
+<?php
 get_footer();
