275f045b417046c6b924eab23ed69ebd2d511c93
Andrew Hudson <ahudson@gmail.com>
2016-09-20 16:57:44 +0100

    change weather api to use api.darksky.net following their rebrand

--- content/plugins/bigandy/api-calls.php
+++ content/plugins/bigandy/api-calls.php
@@ -11,7 +11,7 @@ function ah_set_weather() {
 			// 'timeout' => 120,
 		];
 
-		$response = wp_remote_get( 'https://api.forecast.io/forecast/c3a6795997c7501f0e1e115b3600eb40/51.628284,-1.296668?units=uk', $response_args );
+		$response = wp_remote_get( 'https://api.darksky.net/forecast/c3a6795997c7501f0e1e115b3600eb40/51.628284,-1.296668?units=uk', $response_args );
 
 		// Check if response is an array
 		if ( is_array( $response ) ) {
@@ -26,6 +26,3 @@ function ah_set_weather() {
 
 	return $weather;
 }
-
-
-
