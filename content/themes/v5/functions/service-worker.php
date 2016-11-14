c349c5b0f92f180a8f31d08b95be615e3f80e5db
Andrew Hudson <ahudson@gmail.com>
2016-11-14 20:21:03 +0000

    add dummy image that is big enough for manifest.json

--- content/themes/v5/functions/service-worker.php
+++ content/themes/v5/functions/service-worker.php
@@ -121,7 +121,6 @@ add_action( 'publish_page', 'ah_add_serviceworker_in_root' );
 function ah_add_service_worker_to_footer() {
 	$html = "
 	<script>
-
 		if('serviceWorker' in navigator) {
 			navigator.serviceWorker.register( '" . esc_url( HOMEURL ) . "serviceWorker.js', { scope: '" . esc_url( HOMEURL ) . "' });
 		}
