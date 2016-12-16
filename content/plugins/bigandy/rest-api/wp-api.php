c30661e7baa3235153f200e2e432424913e2d5dc
Andrew Hudson <ahudson@gmail.com>
2016-12-16 12:53:38 +0000

    remove /wp/v2/users endpoint

--- content/plugins/bigandy/rest-api/wp-api.php
+++ content/plugins/bigandy/rest-api/wp-api.php
@@ -73,3 +73,13 @@ function ah_update_meta( $value, $object, $field_name ) {
 
     return update_post_meta( $object->ID, $field_name, $value );
 }
+
+add_filter( 'rest_endpoints', function( $endpoints ){
+    if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+    }
+    if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+    }
+    return $endpoints;
+});
