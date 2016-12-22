a28c9d275cd452c29941845164d233d63808f570
Andrew Hudson <ahudson@gmail.com>
2016-12-22 09:28:12 +0000

    tidy up to use register_meta

--- content/plugins/bigandy/rest-api/wp-api.php
+++ content/plugins/bigandy/rest-api/wp-api.php
@@ -22,72 +22,14 @@ function ah_rest_prepare_post( $data, $post, $request ) {
 }
 add_filter( 'rest_prepare_post', 'ah_rest_prepare_post', 10, 3 );
 
-
-function ah_rest_prepare_health( $data, $post, $request ) {
-	$post_meta = get_post_meta($post->ID);
-
-	$post_id = $post->ID;
-
-	$comments = get_post_meta( $post_id, '_ah_health_comments', true );
-	$weight = get_post_meta( $post_id, '_ah_health_weight', true );
-
-	$_data['_ah_health_comments'] = $weight ? $weight : null;
-	$_data['_ah_health_weight'] = $comments ? $comments : null;
-	$_data['date'] = get_the_date( 'd.m.Y', $post_id );
-
-	$data->data += $_data;
-	return $data;
-}
-// add_filter( 'rest_prepare_health', 'ah_rest_prepare_health', 10, 3 );
-
 $register_string_meta_args = array(
     'type' => 'string',
     'single' => true,
     'show_in_rest' => true,
 );
 
-$register_float_meta_args = array(
-    'type' => 'string',
-    'single' => true,
-    'show_in_rest' => true,
-);
-register_meta( 'post', '_ah_health_weight', $register_float_meta_args );
+register_meta( 'post', '_ah_health_weight', $register_string_meta_args );
 register_meta( 'post', '_ah_health_comments',	$register_string_meta_args );
-register_meta( 'post', 'ah_test', $register_string_meta_args );
-
-// add_action( 'rest_api_init', 'ah_register_weight' );
-function ah_register_weight() {
-	
-  register_rest_field( 'health',
-      '_ah_health_weight',
-      array(
-			'get_callback'    => 'ah_get_meta',
-          'update_callback' => 'ah_update_meta',
-          'schema'          => null,
-      )
-  );
-
-	register_rest_field( 'health',
-      '_ah_health_comments',
-      array(
-			'get_callback'    => 'ah_get_meta',
-          'update_callback' => 'ah_update_meta',
-          'schema'          => null,
-      )
-  );
-}
-
-function ah_get_meta( $object, $field_name, $request ) {
-	return get_post_meta( $object[ 'id' ], $field_name )[0];
-}
-
-function ah_update_meta( $value, $object, $field_name ) {
-  if ( ! $value ) {
-      return;
-  }
-
-  return update_post_meta( $object->ID, $field_name, $value );
-}
 
 add_filter( 'rest_endpoints', function( $endpoints ){
     if ( isset( $endpoints['/wp/v2/users'] ) ) {
