91b88d59450366d3af7ed7e86994f0909d2e4b43
Andrew Hudson <ahudson@gmail.com>
2017-03-16 10:09:16 +0000

    Update admin.php

--- content/themes/v5/functions/admin.php
+++ content/themes/v5/functions/admin.php
@@ -26,14 +26,9 @@ function ba_custom_admin_logo() {
 				clip-path: polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%);
 				color: white;
 				font-weight: bold;
-  			}
-			
-			#login h1 a::after {
 				text-indent: 0;
-				position: absolute;
-				content: "A";
-			}
-			
+  			}
+						
   			#login [type="submit"] {
 				background-color: var(--red);
 				border-color: transparent;
