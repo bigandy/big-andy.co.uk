e5c2f4c96330e9e7deb2a42b44b4aa1a9a14c592
Andrew Hudson <ahudson@gmail.com>
2017-04-10 21:27:05 +0100

    remove changes

--- content/themes/v5/functions/ss.php
+++ content/themes/v5/functions/ss.php
@@ -1,7 +1,7 @@
 <?php
 function ah_md5_file( $file, $prefix ) {
-	$substr = substr( md5( file_get_contents( $file . $prefix ) ), 0, 10 );
-	return $file . '.' . '123456789' . $prefix;
+	// $substr = substr( md5( file_get_contents( $file . $prefix ) ), 0, 10 );
+	return $file . $prefix;
 }
 
 function ah_enqueue_scripts() {
@@ -18,8 +18,6 @@ function ah_enqueue_scripts() {
 	wp_deregister_script( 'wp-embed' );
 
 	if ( is_user_logged_in() ) {
-		echo ah_md5_file( TEMPLATEURI . 'build/js/script', '.js' );
-
 		wp_register_script( 'main', ah_md5_file( TEMPLATEURI . 'build/js/script', '.js' ), false, null, true );
 		wp_enqueue_script( 'main' );
 	}
