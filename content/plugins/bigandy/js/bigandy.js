b5ef72423091ee9e72e84144f1bcc864706cf030
Andrew Hudson <ahudson@gmail.com>
2017-04-12 20:16:48 +0100

    make a style change, run gulp build

--- content/plugins/bigandy/js/bigandy.js
+++ content/plugins/bigandy/js/bigandy.js
@@ -15,6 +15,25 @@
 			$.post(ajaxurl, data, (response) => {
 				console.log('Got this from the server: ' + response);
 			});
+
+			// console.log(ajaxurl);
+			// var http = new XMLHttpRequest();
+			// http.open('POST', ajaxurl, true);
+			// // // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+			//
+			// //Send the proper header information along with the request
+			// http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			// // http.setRequestHeader("Content-length", params.length);
+			// // http.setRequestHeader("Connection", "close");
+			//
+			// http.onreadystatechange = function() {//Call a function when the state changes.
+			// 	console.log(http.status, http.readyState);
+			// 	if (http.readyState == 4 && http.status == 200) {
+			// 		console.log(http.responseText);
+			// 	}
+			// }
+			// http.send(data);
+
 		});
 	});
 })( jQuery );
