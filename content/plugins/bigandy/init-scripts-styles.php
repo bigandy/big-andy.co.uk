86de85aecd27c7737141140dd37873c26ef60cb0
Andrew Hudson <ahudson@gmail.com>
2017-04-13 10:59:38 +0100

    remove need for jquery in bigandy.js

--- content/plugins/bigandy/init-scripts-styles.php
+++ content/plugins/bigandy/init-scripts-styles.php
@@ -7,7 +7,7 @@ function ah_plugin_styles() {
 
 function ah_plugin_scripts() {
 	// Register the script like this for a plugin:
-	wp_register_script( 'ba-script', plugins_url( 'js/bigandy.js', __FILE__ ), 'jquery', false, true );
+	wp_register_script( 'ba-script', plugins_url( 'js/bigandy.js', __FILE__ ), null, false, true );
 	wp_enqueue_script( 'ba-script' );
 }
 add_action( 'admin_enqueue_scripts', 'ah_plugin_styles' );
@@ -15,7 +15,13 @@ add_action( 'admin_enqueue_scripts', 'ah_plugin_scripts' );
 
 // This is included so can use current_user_can() and is_user_logged_in() functions
 include_once( ABSPATH . 'wp-includes/pluggable.php' );
+
+// output the ajaxurl for the bigandy.js script
+function ah_add_ajax_url() {
+	echo '<script> var ajaxurl = "' . admin_url( 'admin-ajax.php' ) . '";</script>';
+}
 // show script if user is logged in and is an admin
 if ( current_user_can( 'update_core' ) && is_user_logged_in() ) {
 	add_action( 'wp_enqueue_scripts', 'ah_plugin_scripts' );
+	add_action( 'wp_head', 'ah_add_ajax_url' );
 }
--- content/plugins/bigandy/js/bigandy.js
+++ content/plugins/bigandy/js/bigandy.js
@@ -1,39 +1,23 @@
-(function($) {
-	$( document ).ready(function() {
-		const button = $('#wp-admin-bar-refresh-service-worker a');
+document.addEventListener("DOMContentLoaded", function(event) {
+	refreshSW();
+});
 
-		button.on('click', (e) => {
-			e.preventDefault();
-			console.log('have been clicked!');
+const refreshSW = () => {
+	const button = document.querySelector('#wp-admin-bar-refresh-service-worker a');
 
-			const data = {
-				'action': 'refresh_serviceworker',
-			};
+	button.addEventListener('click', (e) => {
+		e.preventDefault();
 
-			// when button is clicked fire the post request with action refresh_serviceworker
-			// this meanse that refresh-service-worker.js can call the ah_add_serviceworker_in_root() function
-			$.post(ajaxurl, data, (response) => {
-				console.log('Got this from the server: ' + response);
-			});
+		const xhr = new XMLHttpRequest();
 
-			// console.log(ajaxurl);
-			// var http = new XMLHttpRequest();
-			// http.open('POST', ajaxurl, true);
-			// // // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-			//
-			// //Send the proper header information along with the request
-			// http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			// // http.setRequestHeader("Content-length", params.length);
-			// // http.setRequestHeader("Connection", "close");
-			//
-			// http.onreadystatechange = function() {//Call a function when the state changes.
-			// 	console.log(http.status, http.readyState);
-			// 	if (http.readyState == 4 && http.status == 200) {
-			// 		console.log(http.responseText);
-			// 	}
-			// }
-			// http.send(data);
-
-		});
+		xhr.open('POST', `${ajaxurl}?action=refresh_serviceworker`, true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onload = function() {
+			console.log('Status code: ', xhr.status);
+		    if (xhr.status === 200) {
+		        console.log('Success! ', xhr.responseText);
+		    }
+		};
+		xhr.send();
 	});
-})( jQuery );
+};
