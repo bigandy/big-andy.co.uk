75536d18b8cda70f064a4ec203fbfa6d88f2c6f5
Andrew Hudson <ahudson@gmail.com>
2017-05-20 16:16:25 +0100

    don't use critical css, just use style.css@

--- content/themes/v5/header.php
+++ content/themes/v5/header.php
@@ -10,13 +10,7 @@
 
 	<link rel="manifest" href="<?php echo esc_url( TEMPLATEURI ); ?>manifest.json">
 	<link rel="icon" sizes="192x192" href="<?php echo esc_url( TEMPLATEURI ); ?>images/ba.png">
-	<style><?php
-	if ( is_page() || is_front_page() ) {
-		$css = 'build/css/critical.css';
-	} else {
-		$css = 'build/css/post.css';
-	}
-	include_once( $css ); ?></style>
+	<style><?php include_once( 'style.css' ); ?></style>
 	<?php wp_head(); ?>
 </head>
 <body>
