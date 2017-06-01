5d3051876bc2c4c2e9f621c3e470710fc2d90059
Andrew Hudson <ahudson@gmail.com>
2017-06-01 17:37:39 +0100

    sort out looping

--- content/themes/v5/functions/publish-post.php
+++ content/themes/v5/functions/publish-post.php
@@ -3,14 +3,6 @@
 require 'vendor/autoload.php';
 
 function ah_add_random_image_new_post( $post_id, $post, $update ) {
-
-	// ah_preit( $post_id );
-	// ah_preit( $post );
-	// ah_preit( $update );
-	// echo '<p>status is new' . $new_status . '</p>';
-	// echo '<p>status is old' . $old_status . '</p>';
-	// echo '<p>id is ' . $post->ID . '</p>';
-
 	// Below initialization will create a  phpredis client, or a TinyRedisClient depending on what is installed
 	$emitter = new SocketIO\Emitter(
 		array(
@@ -20,7 +12,7 @@ function ah_add_random_image_new_post( $post_id, $post, $update ) {
 	);
 	// broadcast can be replaced by any of the other flags
 	if ( true === $update ) {
-		$emitter->emit( 'published new post', $post_id, json_encode( $post ), $update );
+		$emitter->emit( 'WordPress published new post', $post_id, json_encode( $post ), $update );
 	}
 }
 add_action( 'save_post', 'ah_add_random_image_new_post', 10, 3 );
@@ -47,26 +39,14 @@ if ( current_user_can( 'update_core' ) && is_user_logged_in() ) {
 				var socket = io('http://localhost:3001');
 				var complete = false;
 
-				socket.on('connect', function(msg) {
-					console.log('i connected');
+				socket.on('connect', () => console.log('connected to socket server'));
+
+
+				socket.on('WordPress published new post', (id, post, update) => {
+					socket.emit('browser published new post', id, post, update );
 				});
 
-				let isDone = false;
-
-				socket.on('post complete', function() {
-					console.log('complete');
-					isDone = true;
-				});
-
-				socket.on('published new post', function(id, post, update) {
-					if (isDone === false) {
-						socket.emit('browser published new post', id, post, update );
-					}
-
-					console.log('is it done yet?', isDone);
-				});
-
-
+				socket.on('the post complete', (msg) => console.log('complete'));
 			</script>
 			<?php
 		}
