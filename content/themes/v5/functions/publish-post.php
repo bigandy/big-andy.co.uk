7ba7b809bedc1057eb7311f51840a87089f3af3f
Andrew Hudson <andrew.hudson@informa.com>
2017-06-06 08:22:32 +0100

    update wp, move conditional into php from socket-server

--- content/themes/v5/functions/publish-post.php
+++ content/themes/v5/functions/publish-post.php
@@ -3,20 +3,25 @@ require 'vendor/autoload.php';
 
 function ah_add_random_image_new_post( $post_id, $post, $update ) {
 	// Below initialization will create a  phpredis client, or a TinyRedisClient depending on what is installed
-	$redis = new Predis\Client(array(
+	$redis = new Predis\Client( array(
 	    'host' => '127.0.0.1',
 	    'port' => 6379
-	));
+	) );
 
 	$data = [
 		id => $post_id,
 		post => $post,
 		update => $update,
-		viaAPI => !is_admin(),
 	];
 
 	// $is_new = $post->post_date === $post->post_modified;
-	$redis->publish( 'WordPress published new post', json_encode( $data ) );
+
+	// if the form has been submitted by API the viaAPI will be true
+	// This will prevent an infinite loop when save_post gets fired every time
+	// either the API or browser editor saves the post.
+	if ( is_admin() ) {
+		$redis->publish( 'WordPress published new post', json_encode( $data ) );
+	}
 }
 add_action( 'save_post', 'ah_add_random_image_new_post', 10, 3 );
 // add_action( 'publish_post', 'wpdocs_run_on_publish_only', 10, 3 );
