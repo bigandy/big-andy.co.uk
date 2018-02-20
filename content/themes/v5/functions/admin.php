c8f7ae984ae400ed0817efd55a59bee93c2d3355
Andrew Hudson <ahudson@gmail.com>
2018-02-20 21:38:06 +0000

    start on ba block

--- content/themes/v5/functions/admin.php
+++ content/themes/v5/functions/admin.php
@@ -1,11 +1,11 @@
 <?php
 // only display custom menu bar if the user is logged in and is an admin
 if ( current_user_can( 'update_core' ) && is_user_logged_in() ) {
-	function ba_admin_css() {
+	function ah_admin_css() {
 		?>
 		<style> #wpadminbar { background: <?php echo esc_attr( HEADERCOLOR ); ?>; }</style>
 		<?php
 	}
-	add_action( 'wp_head', 'ba_admin_css' );
-	add_action( 'admin_head', 'ba_admin_css' );
+	add_action( 'wp_head', 'ah_admin_css' );
+	add_action( 'admin_head', 'ah_admin_css' );
 }
